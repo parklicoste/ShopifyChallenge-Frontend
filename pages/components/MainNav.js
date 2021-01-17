38b21185f53ddfbc2ff571c5a27e389e35aeb1f7
parklicoste <65005378+parklicoste@users.noreply.github.com>
2021-01-16 23:09:37 -0500

    imported movies list as results

--- pages/components/MainNav.js
+++ pages/components/MainNav.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Navbar, InputGroup, FormControl } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MainNav extends React.Component{
 
     constructor(props)
     {
         super(props);
-
         this.state = {
-            search:''
+            name: ""
         }
+    
+    }
+   
+    handleSearch = (event) =>{
+        event.preventDefault();
+        this.props.onMovieChange(this.state.name);
     }
 
     searchChange = event =>{
+        event.preventDefault();
         this.setState({
-          search:event.target.value
+            name: event.target.value
         })
+        
       }
     
 
@@ -26,17 +31,18 @@ class MainNav extends React.Component{
             <nav className="navbar navbar-light bg-light">
                     <div className="container-fluid">
                         {/* <h2 class="navbar-brand ">Movie Name</h2> */}
-                        <form className="d-flex input-group">
-                        <a className="h3 mr-2" style={{color: "black"}}>Movie Name</a>
-                        <input type="text" className="form-control input-text mr-1" placeholder="Search movies...." value={this.state.search} onChange={(e)=>{this.searchChange(e)}} aria-label="Recipient's username" aria-describedby="basic-addon2"/>
-                        <button className="btn btn-outline-success" type="submit" onClick={(e)=>{this.handleSearch(e, this.state.search)}}>Search</button>
+                        <form className="d-flex input-group" onSubmit={(e)=>{this.handleSearch(e)}}>
+                            <a className="h3 mr-2" style={{color: "black"}}>Movie Name</a>
+                            <input type="name" className="form-control input-text mr-1" placeholder="Search movies...." value={this.state.name} onChange={(e)=>{this.searchChange(e)}} aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                            <button className="btn btn-outline-success" type="submit" >Search</button>
                         </form>
                     </div>
                 </nav>
+                {/* <div className="row">
+                <h3>My search is {this.state.name}</h3>
+                </div> */}
             </div>
-            <div className="row">
-                <h3>My search is {this.state.search}</h3>
-            </div>
+
     </div>
 
     }
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import MainNav from './components/MainNav'
-
+import Handler from './components/Handler';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>FrontEnd: Shopify Challenge</title>
-        <link rel="icon" href="logo.ico" />
-        
-      </Head>
-        <div className="container-sm " >
-          <h1 className="display-1 text-bold mt-5">
-            Welcome to the <a href="https://github.com/parklicoste/ShopifyChallenge-Frontend/" target="_blank">Shoppies!</a>
-          </h1>
-          <MainNav/>
-        </div>
-        
-        
-      
-
-      <footer className={styles.footer}>
-        
-      </footer>
-    </div>
-  )
+  return <Handler />
 }
